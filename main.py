db34313720efeee8dbdcbd0a728e51a9fd7e30dd
ahmed-ghaf-404 <alghaf.ahmd@gmail.com>
2025-05-11 12:11:38 -0700

    update

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="RFID Secured Printers API", version="1.0")
 # Allow frontend to access backend (CORS fix)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React frontend URL
+    allow_origins=["http://localhost:3000", "http://192.168.0.139:3000"],  # React frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
